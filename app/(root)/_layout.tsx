5d53d2a508dee9f63c182473f2a367a7fcdddaa1
Naveen Manoj <manojnaveen085@gmail.com>
2025-06-25 17:28:07 +0530

    Profile page setup

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,21 +1,22 @@
-import { useGlobalContext } from "@/lib/global-provider";
 import { Redirect, Slot } from "expo-router";
 import { ActivityIndicator } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { useGlobalContext } from "@/lib/global-provider";
+
 export default function AppLayout() {
-  const { isLogged, loading } = useGlobalContext();
+  const { loading, isLogged } = useGlobalContext();
 
   if (loading) {
     return (
-      <SafeAreaView className="bg-white h-full flex items-center justify-center">
+      <SafeAreaView className="bg-white h-full flex justify-center items-center">
         <ActivityIndicator className="text-primary-300" size="large" />
       </SafeAreaView>
     );
   }
 
   if (!isLogged) {
-    <Redirect href="/sign-in" />;
+    return <Redirect href="/sign-in" />;
   }
 
   return <Slot />;
